603ce5c4368bbd462628b8ca502b890a218a020e
Michael Hazboun <hazbo002@umn.edu>
2024-02-19 10:11:41 -0600

    Added function(x) to the across function. Don't think it's necessary, because it worked without it, but we did it with function(X) in class so IDK.

--- R/week5.R
+++ R/week5.R
@@ -12,12 +12,12 @@ Bnotes_tbl <- read_tsv("../data/Bnotes.txt",col_names = T) #wanted to be fancy a
 Aclean_tbl <- Adata_tbl %>%
   separate(qs, into=paste0("q",1:5)) %>% #i feel like this technically is two verbs/commands in one line (seperate & paste0)
   mutate(datadate=mdy_hms(datadate)) %>%
-  mutate(across(starts_with("q"),as.integer)) %>%
+  mutate(across(starts_with("q"), function(x) as.integer(x))) %>% # I first did it like this,mutate(across(starts_with("q"), as.integer)), I got the right answer, but it's not the way we talked about it in class, is it right?
   left_join(Anotes_tbl,by="parnum") %>%
   filter(is.na(notes))
 ABclean_tbl <- Bdata_tbl %>%
   mutate(datadate=mdy_hms(datadate)) %>%
-  mutate(across(q1:q10,as.integer)) %>% #thought I'd do this one slightly differently for practice
+  mutate(across(q1:q10, function(x) as.integer(x))) %>% #I also did this the first time like this:mutate(across(q1:q10,as.integer)). got the same answer,but unsure if we're supposed to do it that way or not.
   left_join(Bnotes_tbl,by="parnum") %>% 
   filter(is.na(notes)) %>%
   bind_rows(Aclean_tbl, .id= "lab") %>%
